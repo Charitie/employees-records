f008a3a73b6a5b565b47fdffcb6b955852a08e17
charity.tarus <charity.tarus@computercastlesltd.co.ke>
2023-03-27 11:58:06 +0300

    Validate employee details

--- src/employees/employees.routes.js
+++ src/employees/employees.routes.js
@@ -1,14 +1,27 @@
 import { Router } from "express";
+import { validationResult } from "express-validator";
+
 import { asyncRequestHandlerWrapper } from "../lib/asyncRequestHandlerWrapper.js";
+import { employeeValidation } from "../lib/middlewares/employeeValidation.js";
 import { employeeService } from "./employees.service.js";
 
 export function getEmployeeRouter() {
   const employeeRouter = Router();
   employeeRouter.post(
     "/add-employee",
+    employeeValidation,
     asyncRequestHandlerWrapper(async (req, res) => {
+        validationResult(req).throw();
       const employee = await employeeService.addEmployee(req.body);
       res.status(201).json({ message: "Employee added successfully", employee });
+    })
+  );
+
+  employeeRouter.get(
+    "/fetch-employee/:email",
+    asyncRequestHandlerWrapper(async (req, res) => {
+      const employee = await employeeService.getEmployee(req.params.id);
+      res.status(200).json({ message: "success", employee });
     })
   );
 
--- src/employees/employees.service.js
+++ src/employees/employees.service.js
@@ -3,7 +3,16 @@ import { employeeResouce } from "./employees.resource.js";
 
 class EmployeeService {
   async addEmployee(employeeData) {
+    const employeeExist = await employeeResouce.getEmployee(employeeData.email);
+    
+    if (employeeExist) {
+      throw new CustomError("Email already exist", 400);
+    }
     return employeeResouce.addEmployee(employeeData);
+  }
+
+  async getEmployee(email) {
+    return employeeResouce.getEmployee(email);
   }
 }
 
